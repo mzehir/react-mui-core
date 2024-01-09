fda5a76bc020ce1b98b83597f8be98ede3f54145
Mümin <zehir.m.54@gmail.com>
2024-01-09 20:22:40 +0300

    fixed

--- src/components/pageComp/SidebarAppName.js
+++ src/components/pageComp/SidebarAppName.js
@@ -1,31 +1,31 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-import ChipComp from "../Chip";
+// import ChipComp from "../Chip";
 import BoxComp from "../Box";
 // import { APP_NAME } from "../../utils/config/config";
 
-import { green } from "@mui/material/colors";
+// import { green } from "@mui/material/colors";
 import { spacing } from "@mui/system";
 
 import { useSelector } from "react-redux";
 import { generalAppConfigs } from "../../redux/actions/generalAppConfigAction";
 
-const BrandChip = styled(ChipComp)`
-  background-color: ${green[700]};
-  border-radius: 5px;
-  color: ${(props) => props.theme.palette.common.white};
-  font-size: 55%;
-  height: 18px;
-  margin-left: 2px;
-  margin-top: -16px;
-  padding: 3px 0;
+// const BrandChip = styled(ChipComp)`
+//   background-color: ${green[700]};
+//   border-radius: 5px;
+//   color: ${(props) => props.theme.palette.common.white};
+//   font-size: 55%;
+//   height: 18px;
+//   margin-left: 2px;
+//   margin-top: -16px;
+//   padding: 3px 0;
 
-  span {
-    padding-left: ${(props) => props.theme.spacing(1.375)};
-    padding-right: ${(props) => props.theme.spacing(1.375)};
-  }
-`;
+//   span {
+//     padding-left: ${(props) => props.theme.spacing(1.375)};
+//     padding-right: ${(props) => props.theme.spacing(1.375)};
+//   }
+// `;
 
 const BoxCompCustomize = styled(BoxComp)(spacing);
 
@@ -33,7 +33,8 @@ const SidebarAppName = () => {
   const appConfigs = useSelector(generalAppConfigs);
   return (
     <BoxCompCustomize ml={1}>
-      {appConfigs.appName} <BrandChip label="DEMO" />
+      {appConfigs.appName}
+      {/* <BrandChip label="DEMO" /> */}
     </BoxCompCustomize>
   );
 };
--- src/redux/slices/generalAppConfigSlice.js
+++ src/redux/slices/generalAppConfigSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  appName: "App Name",
+  appName: "Startup project",
 };
 
 export const generalAppConfigSlice = createSlice({
